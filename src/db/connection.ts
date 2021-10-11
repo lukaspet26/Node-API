a86f9daed56bc7bb046f45e1983f24d81b6ddec0
lukaspet26 <89996794+lukaspet26@users.noreply.github.com>
2021-10-11 07:46:21 +0200

    setup server and some other updates

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,1 @@
-import { Pool } from 'pg';
-
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: '',
-    database: 'shopdb',
-    port: 5432
-});
+console.log("LATER");
